cd37fd8d4c495fbc27122a3a054f71d4ad6675f7
hbagriacik02 <hbagriacikdev@gmail.com>
2025-04-23 23:44:35 +0200

    Add illegal_log.csv file and update file path

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 #!/bin/python3
 
 import sys
+import datetime
 import pandas as pd
 import ultralytics as yolo
 
 license_plate_input = sys.argv[1]
 illegal_dataFrame = pd.DataFrame(columns=['illegal_traffic_license_plate'])
+
+filepath = '../log/{date}_illegal_log.csv'
+file = filepath.format(date=datetime.datetime.now().strftime('%Y-%m-%d'))
 
 def main():
     print(validate_is_plate_allowed())
@@ -18,12 +22,11 @@ def validate_is_plate_allowed():
     else:
         print("Not Exist")
         illegal_dataFrame.loc[len(illegal_dataFrame)] = license_plate_input
-        illegal_dataFrame.to_csv('./log/illegal_access_denied.csv', index=False)
+        illegal_dataFrame.to_csv(file, index=False)
         return False
 
 def read_csv_access_allowed():
-    return pd.read_csv('access_allowed.csv')
-
+    return pd.read_csv('../access_allowed.csv')
 
 if __name__ == '__main__':
     main()
